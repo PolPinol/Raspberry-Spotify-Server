a9d940219df02a96fa1fc50a9c3b98a042cee3b0
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2021-11-14 17:52:45 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@ sudo apt install raspberrypi-kernel-headers
 sudo apt-get install git
 git clone https://github.com/PolPinol/aso_fase1.git
 cd $HOME/aso_fase1
-make clean
+chmod +777 button1.sh
+chmod +777 button2.sh
+chmod +777 button3.sh
+chmod +777 button4.sh
 make
 sudo insmod led.ko
